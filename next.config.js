57f97ea339d9b090d2aa465696d8988deab9fbd5
ahmedhaggag-a11 <ahmedkhaledhaggag709@gmail.com>
2026-09-20 22:23:20 +0300

    fix: next config for canvas alias with pdfjs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {},
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 }
 
 module.exports = nextConfig
